c3d51379acf140d9ab06486675befaab65fe707e
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-07 14:07:01 +0300

    GH-11-Board#Show-page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,10 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+
+  def show
+    @board = Board.find(params[:id])
+    @cards = @board.cards
+    @action_items = @board.action_items
+  end
 end
