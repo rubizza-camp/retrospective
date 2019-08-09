f10e0690a26d6090c92496fd3efde15f537afa50
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-10 00:16:15 +0300

    Refactor and remove invalid test

--- spec/controllers/action_items_controller_spec.rb
+++ spec/controllers/action_items_controller_spec.rb
@@ -3,20 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ActionItemsController do
-  before(:each) do
-    @board = create(:board)
-  end
+  let(:board) { create(:board) }
 
   context 'POST #create' do
-    it 'redirects if params valid' do
-      post :create, params: { board_id: @board.id,
+    it 'redirects' do
+      post :create, params: { board_id: board.id,
                               action_item: FactoryBot.attributes_for(:action_item) }
-      expect(response).to have_http_status(:redirect)
-    end
-
-    it 'redirects if params not valid' do
-      post :create, params: { board_id: @board.id,
-                              action_item: FactoryBot.attributes_for(:action_item, body: nil) }
       expect(response).to have_http_status(:redirect)
     end
   end
--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -5,20 +5,12 @@ require 'rails_helper'
 RSpec.describe CardsController do
   login_user
 
-  before(:each) do
-    @board = create(:board)
-  end
+  let(:board) { create(:board) }
 
   context 'POST #create' do
-    it 'redirects if params valid' do
-      post :create, params: { board_id: @board.id,
+    it 'redirects' do
+      post :create, params: { board_id: board.id,
                               card: FactoryBot.attributes_for(:card) }
-      expect(response).to have_http_status(:redirect)
-    end
-
-    it 'redirects if params not valid' do
-      post :create, params: { board_id: @board.id,
-                              card: FactoryBot.attributes_for(:card, body: nil) }
       expect(response).to have_http_status(:redirect)
     end
   end
