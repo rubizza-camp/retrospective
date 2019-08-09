2e2e1e426e0c5592418b83ced97d79a49614a0c3
Iris The Fox <iris-miz@yandex.ru>
2019-08-09 11:09:58 +0300

    GH-26: Add User avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,13 @@ class User < ApplicationRecord
   has_many :cards
   has_many :boards
 
+  mount_uploader :avatar, AvatarUploader
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.avatar = auth.info.image
     end
   end
 end
