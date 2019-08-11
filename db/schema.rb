f96e4d692f029fa7e302643f06c9140baa4f66f0
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-12 00:45:03 +0300

    Create teams table and teams_users join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_191643) do
+ActiveRecord::Schema.define(version: 2019_08_11_203918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,19 @@ ActiveRecord::Schema.define(version: 2019_08_08_191643) do
     t.index ["board_id"], name: "index_cards_on_board_id"
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams_users", id: false, force: :cascade do |t|
+    t.bigint "team_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["team_id", "user_id"], name: "index_teams_users_on_team_id_and_user_id"
+    t.index ["user_id", "team_id"], name: "index_teams_users_on_user_id_and_team_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -62,4 +75,6 @@ ActiveRecord::Schema.define(version: 2019_08_08_191643) do
   add_foreign_key "boards", "users", column: "creator_id"
   add_foreign_key "cards", "boards"
   add_foreign_key "cards", "users", column: "author_id"
+  add_foreign_key "teams_users", "teams"
+  add_foreign_key "teams_users", "users"
 end
