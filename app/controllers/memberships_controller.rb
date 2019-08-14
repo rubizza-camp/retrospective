0c7b11d043cdf61f2b6be694ad1d4c20b19167f8
Iris The Fox <iris-miz@yandex.ru>
2019-08-14 18:28:40 +0300

    change to select tag

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 class MembershipsController < ApplicationController
+  before_action :set_board
+
   def invite
-    @board = Board.find(params[:id])
-    email = params[:membership][:email]
-    if (user = User.find_by(email: email))
-      @membership = Membership.new(board_id: @board.id, user_id: user.id, role: 'member')
+    id = params[:membership][:id]
+    user = User.find(id)
+    @membership = Membership.new(board_id: @board.id, user_id: user.id, role: 'member')
 
-      if @membership.save
-        redirect_to @board, notice: 'Member was successfully created.'
-      else
-        redirect_to @board, notice: 'Member WAS NOT successfully created.'
-      end
-
+    if @membership.save
+      redirect_to @board, notice: 'Member was successfully created.'
     else
-      redirect_to @board, notice: 'User with this email not exists'
+      redirect_to @board, notice: 'Member WAS NOT successfully created.'
     end
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
   end
 end
