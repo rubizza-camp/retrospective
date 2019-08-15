d77c28d28a4eac77abc5b68015de21f5075bfaad
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-15 18:10:53 +0300

    Refactor memberships controller & boards show view

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class MembershipsController < ApplicationController
+  before_action :authenticate_user!, :set_board
+
   def create
-    authenticate_user!
-    @board = Board.find(params[:board_id])
     @membership = @board.memberships.build(user_id: current_user.id, role: :member)
 
     if @membership.save
-      redirect_to @board, notice: "#{current_user.email} user has joined the board!"
+      redirect_to @board, notice: "#{current_user.email.split('@').first} has joined the board!"
     else
       redirect_to @board, alert: @membership.errors.full_messages.join(', ')
     end
+  end
+
+  private
+
+  def set_board
+    @board = Board.find(params[:board_id])
   end
 end
