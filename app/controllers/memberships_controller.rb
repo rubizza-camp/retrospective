8326eb40dedc7cf181fd93468c0d8a0f2522f648
Iris The Fox <iris-miz@yandex.ru>
2019-08-15 11:49:01 +0300

    Add strong params

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,15 +4,19 @@ class MembershipsController < ApplicationController
   before_action :set_board
 
   def invite
-    id = params[:membership][:id]
-    user = User.find(id)
-    @membership = Membership.new(board_id: @board.id, user_id: user.id, role: 'member')
+    @membership = Membership.new(membership_params.merge(board_id: @board.id, role: 'member'))
 
     if @membership.save
       redirect_to @board, notice: 'Member was successfully created.'
     else
       redirect_to @board, notice: 'Member WAS NOT successfully created.'
     end
+  end
+  
+  private
+
+  def membership_params
+    params.require(:membership).permit(:user_id)
   end
 
   def set_board
