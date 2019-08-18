9e2bd556c59a3c3f70ebd83c3070d7b4a0d1501d
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-18 15:33:00 +0300

    React hello world

---
+++ app/javascript/packs/ready_button_component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <div>Hello world!</div>,
+      document.body.appendChild(document.createElement('div')),
+    )
+})
+        
