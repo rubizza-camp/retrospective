e7e4c0ff3852f00c63d06292c78c50444ec0b242
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-19 04:25:53 +0300

    Fetch ready status with button onClick

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MembershipsController < ApplicationController
-  before_action :authenticate_user!, :set_board
+  before_action :authenticate_user!, :set_board, except: :ready
 
   def create
     @membership = @board.memberships.build(user_id: current_user.id, role: :member)
@@ -13,12 +13,17 @@ class MembershipsController < ApplicationController
     end
   end
 
-  def toggle_ready
+  def ready
     #if (membership = @board.memberships.find_by(user_id: current_user.id))
     #  membership.update(ready: !membership.ready)
     #  render json: membership.ready
     #end
-    render json: current_user
+    render plain: 'yoyo this is memberships/ready'
+  end
+
+  # for now this returns current_user ready status
+  def new
+    render json: @board.memberships.find_by(user_id: current_user.id).ready
   end
 
   private
--- app/javascript/packs/ready_button_component.jsx
+++ app/javascript/packs/ready_button_component.jsx
@@ -4,15 +4,39 @@ import ReactDOM from 'react-dom'
 export class ReadyButton extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      ready: null
+    };
+    this.handleClick = this.handleClick.bind(this)
   }
-render() {
-  return (
-    <button className='button is-large'>React Ready Button</button>
+
+  handleClick() {
+    fetch(`http://localhost:3000/boards/1/memberships/new`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          ready : result
+        });
+      },
+    )
+  }
+  
+  /*
+  componentDidMount() {
+    const response = fetch(`http://localhost:5000/memberships/toggle_ready`).then(res => res.json()); 
+      this.setState({
+      ready: fetch(`http://localhost:5000/memberships/toggle_ready`)
+    });
+  }
+  */
+
+  render() {
+    return (
+        <button className='button is-large' onClick={this.handleClick}>READY</button>
     );
   }
 };
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
