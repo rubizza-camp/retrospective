3d794b9a4a6b4d15260970ba329379e1459828cf
Iris The Fox <iris-miz@yandex.ru>
2019-08-20 16:43:02 +0300

    fix rubocop

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -6,13 +6,12 @@ module API
     before_action :set_board, only: :invite
 
     # rubocop: disable Metrics/MethodLength
-    # rubocop: disable Metrics/AbcSize
     def invite
       user = User.find_by(email: board_params[:email])
       if user
         membership = @board.memberships.build(role: 'member', user_id: user.id)
         if membership.save
-          render json: {email: user.email}
+          render json: { email: user.email }
         else
           render json: { error: membership.errors.full_messages.join(',') }, status: 400
         end
@@ -20,7 +19,6 @@ module API
         render json: { error: 'User was not found' }, status: 400
       end
     end
-    # rubocop: enable Metrics/AbcSize
     # rubocop: enable Metrics/MethodLength
 
     private
