a2c56a197a127623b88ec64dade6f49a9d8d4f8f
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-21 17:41:04 +0300

    Add before board create slug generation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
+require 'nanoid'
+
 class Board < ApplicationRecord
   has_many :action_items
   has_many :cards
   has_many :memberships
   has_many :users, through: :memberships
   validates_presence_of :title
+
+  before_create :set_slug  
+  
+  private  
+  
+  def set_slug
+    #loop do
+      self.slug = Nanoid.generate(size: 10)
+      #break unless Board.where(slug: slug).exists?
+    #end
+  end
 end
