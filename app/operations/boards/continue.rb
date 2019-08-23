2c59e24dcbac4657750e77d8501bf252b94bcc14
Iris The Fox <iris-miz@yandex.ru>
2019-08-23 10:58:06 +0300

    Memberships dup

--- app/operations/boards/continue.rb
+++ app/operations/boards/continue.rb
@@ -9,25 +9,18 @@ module Boards
       @prev_board = prev_board
     end
 
-    # rubocop: disable Metrics/AbcSize
-    # rubocop: disable Metrics/MethodLength
     def call
       new_board = Board.new(
         title: default_board_name,
         previous_board_id: prev_board.id
       )
-      prev_board.memberships.each do |member|
-        new_board.memberships.build(user_id: member.user_id,
-                                    role: member.role)
-      end
+      new_board.memberships = prev_board.memberships.map(&:dup)
       new_board.save
 
       Result.new(value: new_board, success?: true)
     rescue StandardError => e
       Result.new(error: e.message, success?: false)
     end
-    # rubocop: enable Metrics/AbcSize
-    # rubocop: enable Metrics/MethodLength
 
     def default_board_name
       Date.today.strftime('%d-%m-%Y')
