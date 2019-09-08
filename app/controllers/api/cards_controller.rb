daf38fda7ed2becdc24e03203cc67a0393af7caa
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-09-09 00:04:45 +0300

    modify api/cards_controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -2,28 +2,30 @@
 
 module API
   class CardsController < ApplicationController
-
     before_action :set_board
     before_action :set_card
-  
+
     rescue_from ActionPolicy::Unauthorized do |ex|
       redirect_to @board, alert: ex.result.message
     end
 
     def destroy
       authorize! @card
-      @card.destroy   
       respond_to do |format|
-        format.json { head :ok }
+        if @card.destroy
+          format.json { head :ok }
+        else
+          format.json { redirect_to @board, alert: 'Card delete sequence failed' }
+        end
       end
     end
 
     private
-  
+
     def set_board
       @board = Board.find_by!(slug: params[:board_slug])
     end
-  
+
     def set_card
       @card = Card.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
@@ -31,3 +33,5 @@ Rails.application.routes.draw do
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
