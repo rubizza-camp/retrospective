45c991be223820840b495dc94d9291eafe8b94d3
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-09-09 18:02:19 +0300

    add Body component

--- app/javascript/components/card/Body.jsx
+++ app/javascript/components/card/Body.jsx
@@ -3,6 +3,13 @@ import React from "react"
 class Body extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { body: this.props.body };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('submit happened');
   }
 
   render () {
@@ -11,8 +18,11 @@ class Body extends React.Component {
     return (
       <>
         {/*<div onClick={ ()=> { editable ? console.log('editable') : console.log('non-editable') }}>{body}</div>*/}
-        <div>{body}</div>
-        <input></input>
+        
+        <div>{this.state.body}</div>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' defaultValue={this.state.body} />
+        </form>
       </>
     );
   }
