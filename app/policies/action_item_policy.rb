8894deac9f443bbe20f8c179cc429bbd32cf9926
iris <iris-miz@yandex.ru>
2019-09-11 14:23:13 +0300

    add move action

--- app/policies/action_item_policy.rb
+++ app/policies/action_item_policy.rb
@@ -2,6 +2,19 @@
 
 class ActionItemPolicy < ApplicationPolicy
   def create?
+    check?(:user_is_member)
+  end
+
+  def move?
+    check?(:user_is_creator)
+  end
+
+  def user_is_creator
+    check?(:user_is_member) &&
+      user.memberships.find_by(board_id: record.board.id).role == 'creator'
+  end
+
+  def user_is_member
     record.board.users.include?(user)
   end
 end
