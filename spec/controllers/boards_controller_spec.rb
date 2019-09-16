1b6a24376d20ae5812f3d6afd74705d0b1dcfee2
iris <iris-miz@yandex.ru>
2019-09-16 11:20:21 +0300

    fix specs

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -3,11 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe BoardsController do
-  before do
-    login_user
-  end
-
   let_it_be(:board) { create(:board) }
+  let(:current_user) { create(:user) }
+  before { login_as current_user }
 
   context 'GET #index' do
     it 'returns http success' do
