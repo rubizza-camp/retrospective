3f1d5afcbeee88ef5542bca16d59f9896e19f8bf
iris <iris-miz@yandex.ru>
2019-09-18 12:51:48 +0300

    fix membership policy spec

--- spec/policies/api/membership_policy_spec.rb
+++ spec/policies/api/membership_policy_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe API::MembershipPolicy do
   let_it_be(:failed_policy) { described_class.new(membership, user: not_a_member) }
   let_it_be(:creator_policy) { described_class.new(creatorship, user: creator) }
 
-  context '#ready_status?' do
+  describe '#ready_status?' do
     context 'when user is a member' do
       it {  expect(member_policy.apply(:ready_status?)).to eq true }
     end
@@ -24,7 +24,7 @@ RSpec.describe API::MembershipPolicy do
     end
   end
 
-  context '#ready_toggle?' do
+  describe '#ready_toggle?' do
     context 'when user is a member' do
       it {  expect(member_policy.apply(:ready_toggle?)).to eq true }
     end
@@ -33,7 +33,7 @@ RSpec.describe API::MembershipPolicy do
     end
   end
 
-  context '#destroy?' do
+  describe '#destroy?' do
     context 'when user is a creator' do
       it {  expect(creator_policy.apply(:destroy?)).to eq true }
     end
@@ -42,7 +42,7 @@ RSpec.describe API::MembershipPolicy do
     end
   end
 
-  context '#user_is_member?' do
+  describe '#user_is_member?' do
     context 'when user is a member' do
       it {  expect(member_policy.apply(:user_is_member?)).to eq true }
     end
@@ -51,7 +51,7 @@ RSpec.describe API::MembershipPolicy do
     end
   end
 
-  context '#user_is_creator?' do
+  describe '#user_is_creator?' do
     context 'when user is a creator' do
       it {  expect(creator_policy.apply(:user_is_creator?)).to eq true }
     end
