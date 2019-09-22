47230e534f6df193c23fb4ef2bb5e993cca8871b
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-09-22 20:14:56 +0300

    add edit and delete actions for boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class BoardsController < ApplicationController
-  before_action :set_board, only: %i[show continue]
+  before_action :set_board, only: %i[show continue edit update destroy]
   skip_before_action :authenticate_user!, only: :show
   skip_verify_authorized
 
   def index
-    @boards = Board.all
+    @boards = current_user.boards.sort_by(&:created_at)
   end
 
   # rubocop: disable Metrics/AbcSize
@@ -26,6 +26,9 @@ class BoardsController < ApplicationController
     @board = Board.new(title: Date.today.strftime('%d-%m-%Y'))
   end
 
+  def edit
+  end
+
   def create
     @board = Board.new(board_params)
     @board.memberships.build(user_id: current_user.id, role: 'creator')
@@ -34,6 +37,22 @@ class BoardsController < ApplicationController
       redirect_to @board, notice: 'Board was successfully created.'
     else
       render :new
+    end
+  end
+
+  def update
+    if @board.update(board_params)
+      redirect_to boards_path, notice: 'Board was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @board.destroy
+      redirect_to boards_path, notice: 'Board was successfully deleted.'
+    else
+      redirect_to boards_path, alert: @board.errors.full_messages.join(', ')
     end
   end
 
