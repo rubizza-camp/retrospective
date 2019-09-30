55dcd18f8604d544803ee690a2fe53be74f4f7b2
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-09-30 11:43:22 +0300

    continue opp spec wip

--- spec/operations/continue_spec.rb
+++ spec/operations/continue_spec.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Boards::Continue do
   let_it_be(:creator) { create(:user) }
   let_it_be(:prev_board) { create(:board) }
   let_it_be(:creatorship) { create(:membership, board: prev_board, user: creator, role: 'creator') }
-
-  #subject { described_class.new(board, user) }
 
   describe '#default_board_name' do
     subject { described_class.new(prev_board, creator).send(:default_board_name) }
@@ -24,20 +24,5 @@ RSpec.describe Boards::Continue do
       let_it_be(:next_board) { create(:board, previous_board: prev_board) }
       it { is_expected.to eq true }
     end
-  end
-
-  describe '#duplicate_memberships' do
-    subject { described_class.new(prev_board, creator).send(:duplicate_memberships) }
-
-    let_it_be(:member) { create(:user) }
-    let_it_be(:membership) { create(:membership, board: prev_board, user: member) }
-
-    it { is_expected.to be_instance_of(Array) }
-    #it { is_expected.to  }
-
-
-    #it { binding.pry }
-      
-    #end
   end
 end
