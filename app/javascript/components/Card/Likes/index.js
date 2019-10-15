28c0fa1184002978df5983a0d7b200a877715525
iris <iris-miz@yandex.ru>
2019-10-15 12:23:06 +0300

    add likes to cards

---
+++ app/javascript/components/Card/Likes/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+class Likes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: this.props.likes
+    };
+  }
+  
+  handleClick = () => {
+    fetch(`/api/${window.location.pathname}/cards/${this.props.id}/like`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector("meta[name='csrf-token']").getAttribute('content')
+      }
+    }).then((result) => {
+      if (result.status == 200) {
+        result.json()
+        .then((resultHash) => {
+          this.setState({likes: resultHash.likes});
+        })
+      }
+      else { 
+        throw result
+      }
+    }).catch((error) => {
+        error.json()
+        .then((errorHash) => {
+          console.log(errorHash.error)
+        })
+    });
+  }
+
+  render () {
+    const likes = this.state.likes
+    return (
+      <React.Fragment>
+        <a className='button has-text-info' onClick={this.handleClick}>
+          <i className="fa fa-heart is-info"></i>
+          <span> {likes} </span>
+        </a>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Likes
