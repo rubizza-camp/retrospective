15e181b4dd08b57a4e568e4dd3b4d00179bb0b9f
gaev-art <forsakensr@gmail.com>
2021-08-20 11:06:04 +0300

    fix api requests

--- app/javascript/components/board-card/menu-icon/menu-icon.jsx
+++ app/javascript/components/board-card/menu-icon/menu-icon.jsx
@@ -2,45 +2,16 @@ import React, {useState} from 'react';
 import style from './menu-icon.module.less';
 import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-// Import {useMutation} from 'react-apollo';
-// import {destroyBoardMutation, continueBoardMutation} from '../operations.gql';
 import {api} from '../../api/api';
 
 export const MenuIcon = ({boardSlug}) => {
-  // Const [destroyBoard] = useMutation(destroyBoardMutation);
-  // const [continueBoard] = useMutation(continueBoardMutation);
-  // const deleteBoard = async () => {
-  //   const {data} = await destroyBoard({
-  //     variables: {
-  //       boardSlug
-  //     }
-  //   });
-
-  //   if (data.destroyBoard.boardSlug) {
-  //     console.log(data.destroyBoard.errors.fullMessages.join(' '));
-  //   }
-  // };
-
-  // const addBoard = async () => {
-  //   const {data} = await continueBoard({
-  //     variables: {
-  //       boardSlug
-  //     }
-  //   });
-
-  //   if (data.continueBoard.boardSlug) {
-  //     console.log(data.continueBoard.errors.fullMessages.join(' '));
-  //   }
-  // };
-
   const [isOpen, setIsOpen] = useState(false);
 
   const continueHandler = () => {
-    // AddBoard()
     api
       .post(`boards/${boardSlug}/continue`)
       .then((response) => console.log(response));
-    alert('board has been added');
+
     setIsOpen(false);
   };
 
@@ -49,15 +20,16 @@ export const MenuIcon = ({boardSlug}) => {
       .get(`boards/${boardSlug}/history`)
       .then((response) => console.log(response));
     api.get(`boards`).then((response) => console.log(response));
-    alert('history');
+
     setIsOpen(false);
   };
 
   const deleteHandler = () => {
-    // DeleteBoard()
-    api.delete(`boards/${boardSlug}`).then((response) => console.log(response));
+    api
+      .delete(`boards/${boardSlug}`, {}, {})
+      .then((response) => console.log(response));
+
     setIsOpen(false);
-    alert('board was deleted');
   };
 
   return (
