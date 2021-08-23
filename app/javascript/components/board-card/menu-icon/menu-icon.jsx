10ad22305d9150e217241860d8a511fbcbb09c59
gaev-art <forsakensr@gmail.com>
2021-08-23 13:13:40 +0300

    replaced the boardsApi with the boards-api

--- app/javascript/components/board-card/menu-icon/menu-icon.jsx
+++ app/javascript/components/board-card/menu-icon/menu-icon.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import style from './menu-icon.module.less';
 import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {boardApi} from '../../api/boardsApi';
+import {boardApi} from '../../api/boards-api';
 
 export const MenuIcon = ({boardSlug}) => {
   const [isOpen, setIsOpen] = useState(false);
