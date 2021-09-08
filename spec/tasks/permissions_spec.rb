5c44db7c7e0b34cfdeadc8c30e4d776eb3c235a8
Julia Lugina <julia.lugina@cybergizer.com>
2021-09-08 13:58:52 +0300

    Add spec for rake tasks

--- spec/tasks/permissions_spec.rb
+++ spec/tasks/permissions_spec.rb
@@ -82,7 +82,62 @@ RSpec.describe 'permissions.rake' do
         expect(user.comment_permissions).to include(comment_permission)
       end
 
-      it 'does not build non card permissions' do
+      it 'does not build non comment permissions' do
+        run_task
+        expect(user.comment_permissions).not_to include(other_permission)
+      end
+    end
+
+    context "for card's likes" do
+      let_it_be(:like_permission) { create(:permission, identifier: 'like_card') }
+      let(:author) { create(:user) }
+      let!(:card) { create(:card, board: board, author: author) }
+      let_it_be(:task_name) { 'permissions:create_missing_for_like_cards' }
+
+      before do
+        create(:membership, user: user, board: board)
+        create(:membership, user: author, board: board)
+      end
+
+      it 'builds missing like_card permission' do
+        run_task
+        expect(user.card_permissions).to include(like_permission)
+      end
+
+      it 'does not build like_card permission for author' do
+        run_task
+        expect(author.card_permissions).not_to include(like_permission)
+      end
+
+      it 'does not build non card permission' do
+        run_task
+        expect(user.card_permissions).not_to include(other_permission)
+      end
+    end
+
+    context "for comment's likes" do
+      let_it_be(:like_permission) { create(:permission, identifier: 'like_comment') }
+      let(:author) { create(:user) }
+      let(:card) { create(:card, board: board) }
+      let!(:comment) { create(:comment, author: author, card: card) }
+      let_it_be(:task_name) { 'permissions:create_missing_for_like_comments' }
+
+      before do
+        create(:membership, user: user, board: board)
+        create(:membership, user: author, board: board)
+      end
+
+      it 'builds missing like_comment permission' do
+        run_task
+        expect(user.comment_permissions).to include(like_permission)
+      end
+
+      it 'does not build like_comment permission for author' do
+        run_task
+        expect(author.comment_permissions).not_to include(like_permission)
+      end
+
+      it 'does not build non comment permission' do
         run_task
         expect(user.comment_permissions).not_to include(other_permission)
       end
