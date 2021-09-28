7e70704e325c7d6c7efe5babc780add228bbabf0
Julia Lugina <julia.lugina@cybergizer.com>
2021-09-28 11:03:54 +0300

    Add assignee to ActionItem serializer

--- app/serializers/action_item_serializer.rb
+++ app/serializers/action_item_serializer.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class ActionItemSerializer < ActiveModel::Serializer
-  attributes :id, :body, :times_moved, :status, :assignee_avatar_url
-  belongs_to :author
+  attributes :id, :body, :times_moved, :status
 
-  def assignee_avatar_url
-    object.assignee.avatar.thumb.url if object.assignee
-  end
+  belongs_to :author
+  belongs_to :assignee
 end
