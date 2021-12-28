91ca201fbc86f09b0e5b82ffc084e2481a50b87e
Yauhen Vinkevich <32642822+yauvink@users.noreply.github.com>
2021-12-28 14:41:53 +0300

    Add dragNdrop lib, change cards table (#509)
    Add dragNdrop lib, change cards table

--- app/javascript/components/action-items-container.jsx
+++ app/javascript/components/action-items-container.jsx
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from 'react';
+import {Droppable, Draggable, DragDropContext} from 'react-beautiful-dnd';
+import uuid from 'uuid';
+
 import {ActionItem} from './action/action-item';
 import {actionItemsApi} from './api/action-items-api';
 import './style.less';
 
 const ActionItemsContainer = () => {
   const [actionItems, setActionItems] = useState([]);
+  const columns = ['pending', 'inProgress', 'done'];
 
   useEffect(() => {
     actionItemsApi
@@ -12,13 +16,66 @@ const ActionItemsContainer = () => {
       .then((response) => setActionItems(response));
   }, []);
 
+  const handleOnDragEnd = (items) => (result) => {
+    const {destination} = result;
+
+    if (!destination) {
+      return;
+    }
+
+    console.log(items, result);
+  };
+
+  const getColumnName = (name) => {
+    switch (name) {
+      case 'pending':
+        return 'To do';
+      case 'inProgress':
+        return 'In progress';
+      case 'done':
+        return 'Done';
+    }
+  };
+
   return (
     <div className="items-container">
-      {actionItems.map((item) => {
-        return (
-          item.status !== 'closed' && <ActionItem key={item.id} item={item} />
-        );
-      })}
+      <DragDropContext onDragEnd={handleOnDragEnd(actionItems)}>
+        {columns.map((columnName) => (
+          <div key={uuid()} className="items-column">
+            <span className="column-header">{getColumnName(columnName)}</span>
+            <Droppable droppableId={columnName}>
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {actionItems.map((item) => {
+                    if (item.status === columnName) {
+                      return (
+                        <Draggable
+                          key={uuid()}
+                          draggableId={String(item.id)}
+                          index={item.id}
+                        >
+                          {(provided) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <ActionItem key={uuid()} item={item} />
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    }
+
+                    return null;
+                  })}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        ))}
+      </DragDropContext>
     </div>
   );
 };
