2bd57a6954af8bf0e4f08da799355af25d911324
YarikHrom <yar.horoshev2@gmail.com>
2022-02-22 00:56:53 +0700

    Add board_id and board_title to ActionItem

--- app/serializers/action_item_serializer.rb
+++ app/serializers/action_item_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ActionItemSerializer < ActiveModel::Serializer
-  attributes :id, :body, :times_moved, :status
+  attributes :id, :body, :times_moved, :status, :board_id, :board_title
 
   belongs_to :author
   belongs_to :assignee
