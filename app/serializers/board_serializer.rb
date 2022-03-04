74b0675352d79f7fe47b5d889f7f8bdf60f0a116
YarikHrom <yar.horoshev2@gmail.com>
2022-03-04 18:03:10 +0700

    Replace next_board and previous_board with &.slug

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -4,7 +4,7 @@ class BoardSerializer < ActiveModel::Serializer
   USERS_LIMIT = 4
 
   # rubocop:disable Layout/LineLength
-  attributes :id, :title, :slug, :created_at, :users_count, :private, :boards_count, :column_emojis, :next_board, :previous_board
+  attributes :id, :title, :slug, :created_at, :users_count, :private, :boards_count, :column_emojis, :next_board_slug, :previous_board_slug
   # rubocop:enable Layout/LineLength
   attribute :users, if: :include_users?
   attribute :previous_action_items
@@ -15,6 +15,22 @@ class BoardSerializer < ActiveModel::Serializer
       []
     else
       object.previous_board.action_items
+    end
+  end
+
+  def previous_board_slug
+    if object.previous_board.nil?
+      object.previous_board&.slug = null
+    else
+      object.previous_board&.slug
+    end
+  end
+  
+  def next_board_slug
+    if object.next_board.nil?
+      object.next_board&.slug = null
+    else
+      object.next_board&.slug
     end
   end
 
