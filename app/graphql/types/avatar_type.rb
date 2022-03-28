d38fea652b43f978f5b8847e37690222b1868c2f
YarikHrom <yar.horoshev2@gmail.com>
2022-03-28 18:53:42 +0700

    Detele graphql from project

--- app/graphql/types/avatar_type.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class AvatarType < Types::BaseObject
-    field :thumb, Types::ThumbType, null: true
-  end
-end
--- app/graphql/types/suggestion_type.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class SuggestionType < Types::BaseObject
-    field :users, [String], null: false
-    field :teams, [String], null: false
-  end
-end
