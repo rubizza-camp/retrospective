baa1441bbdee00246962d65765d8c77ba5a4f80b
YarikHrom <yar.horoshev2@gmail.com>
2022-07-04 03:09:33 +0700

    Rewrite CommentsChannel

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -2,12 +2,7 @@
 
 class CommentsChannel < ApplicationCable::Channel
   def subscribed
-    params.deep_transform_keys!(&:underscore)
-    board = Board.find_by(slug: params[:board_slug])
-
-    return reject unless board
-
-    stream_for board
+    stream_from 'comment'
   end
 
   def unsubscribed
